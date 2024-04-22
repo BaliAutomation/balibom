36843ceee0bf508049e9e3c34b9cb2c2ac80e8ad
niclas <niclas@hedhman.org>
2024-04-22 18:40:58 +0200

    It should have been File.separator, not pathSeparator

--- src/main/java/ac/bali/bom/products/ProductsService.java
+++ src/main/java/ac/bali/bom/products/ProductsService.java
@@ -57,7 +57,7 @@ public interface ProductsService
         static String parseNameFromFile(File bomFile)
         {
             String path = bomFile.getAbsolutePath();
-            String[] parts = path.split(File.pathSeparator);
+            String[] parts = path.split(File.separator);
             return parts[parts.length - 3];
         }
 
